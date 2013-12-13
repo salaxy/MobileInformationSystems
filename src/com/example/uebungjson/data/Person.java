6cddd6974f860e66e9ff2f33c2687a590959c456
kaain <ott@fh-brandenburg.de>
2013-12-13 15:25:47 +0100

    json parser + person object

---
+++ src/com/example/uebungjson/data/Person.java
@@ -0,0 +1,12 @@
+package com.example.uebungjson.data;
+
+import java.util.Date;
+
+public class Person {
+
+    long id;
+    String vorname;
+    String nachname;
+    String geschlecht;
+    Date bday;
+}
